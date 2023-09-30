03932fa0ad1c912f9b0918ba3afda877f757db15
MahmoudEl-Husseni <oudm753@gmail.com>
2023-09-30 22:21:23 +0300

    Header file added

---
+++ Drive Modes/Drive Modes.h
@@ -0,0 +1,15 @@
+/*
+ * Drive_Modes.h
+ *
+ *      Author: Ziad Mahmoud Saad
+ */
+
+#ifndef DRIVE_MODES_H_
+#define DRIVE_MODES_H_
+
+void repeat();
+void obstacle_avoiding();
+void obstacel_avoiding_with_recording();
+void Drive();
+void repeat_Drive();
+#endif /* DRIVE_MODES_H_ */
